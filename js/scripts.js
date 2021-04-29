d2cc837a160bad3430d91f47241a82cbcde2ab3f
Emmanuel Carmona <pinombole@gmail.com>
2021-04-29 04:33:35 -0300

    Terminada seccion Mono Mario en Animacion

--- js/scripts.js
+++ js/scripts.js
@@ -293,3 +293,184 @@ jQuery(document).ready(function() {
 });
 
 
+/* Carousel Multi Item Mono*/	
+jQuery(document).ready(function() {		
+	$('#gallery-Mono').on('slide.bs.carousel', function (e) {
+	    /*
+	        CC 2.0 License Iatek LLC 2018
+	        Attribution required
+	    */
+	    var $e = $(e.relatedTarget);
+	    var idx = $e.index();
+	    var itemsPerSlide = 4;
+	    var totalItems = $('#gallery-Mono .carousel-item').length;
+	    
+	    if (idx >= totalItems-(itemsPerSlide-1)) {
+	        var it = itemsPerSlide - (totalItems - idx);
+	        for (var i=0; i<it; i++) {
+	            // append slides to end
+	            if (e.direction=="left") {
+	                $('#gallery-Mono .carousel-item').eq(i).appendTo('#gallery-Mono .carousel-inner');
+	            }
+	            else {
+	                $('#gallery-Mono .carousel-item').eq(0).appendTo('#gallery-Mono .carousel-inner');
+	            }
+	        }
+	    }
+	});
+	
+});
+
+//Carrousel Modal Mono//
+jQuery(document).ready(function() { 
+    /*
+        Stop video playing when the MODAL is being closed (has finished closing)
+    */
+    $('#modal-Mono').on('hidden.bs.modal', function(e) {
+        $('#modal-Mono iframe').each(function() {
+            var videoURL = $(this).attr('src');
+            $(this).attr('src', videoURL);
+        });
+    });
+ 
+});
+
+jQuery(document).ready(function() { 
+    /*
+        Stop video playing when the CAROUSEL slides to another element
+    */
+    $('#carousel-Mono').on('slid.bs.carousel', function(e) {
+        var currentSlide = $('#carousel-Mono .carousel-item').eq(e.from);
+        var currentSlideEmbed = currentSlide.children('.embed-responsive');
+        if(currentSlideEmbed.length > 0) {
+            var videoIFrame = currentSlideEmbed.children('iframe');
+            var videoURL = videoIFrame.attr('src');
+            videoIFrame.attr('src', videoURL);
+        }
+    });
+ 
+});
+
+
+/* Carousel Multi Item Fort*/	
+jQuery(document).ready(function() {		
+	$('#gallery-Fort').on('slide.bs.carousel', function (e) {
+	    /*
+	        CC 2.0 License Iatek LLC 2018
+	        Attribution required
+	    */
+	    var $e = $(e.relatedTarget);
+	    var idx = $e.index();
+	    var itemsPerSlide = 4;
+	    var totalItems = $('#gallery-Fort .carousel-item').length;
+	    
+	    if (idx >= totalItems-(itemsPerSlide-1)) {
+	        var it = itemsPerSlide - (totalItems - idx);
+	        for (var i=0; i<it; i++) {
+	            // append slides to end
+	            if (e.direction=="left") {
+	                $('#gallery-Fort .carousel-item').eq(i).appendTo('#gallery-Fort .carousel-inner');
+	            }
+	            else {
+	                $('#gallery-Fort .carousel-item').eq(0).appendTo('#gallery-Fort .carousel-inner');
+	            }
+	        }
+	    }
+	});
+	
+});
+
+//Carrousel Modal Fort//
+jQuery(document).ready(function() { 
+    /*
+        Stop video playing when the MODAL is being closed (has finished closing)
+    */
+    $('#modal-Fort').on('hidden.bs.modal', function(e) {
+        $('#modal-Fort iframe').each(function() {
+            var videoURL = $(this).attr('src');
+            $(this).attr('src', videoURL);
+        });
+    });
+ 
+});
+
+jQuery(document).ready(function() { 
+    /*
+        Stop video playing when the CAROUSEL slides to another element
+    */
+    $('#carousel-Fort').on('slid.bs.carousel', function(e) {
+        var currentSlide = $('#carousel-Fort .carousel-item').eq(e.from);
+        var currentSlideEmbed = currentSlide.children('.embed-responsive');
+        if(currentSlideEmbed.length > 0) {
+            var videoIFrame = currentSlideEmbed.children('iframe');
+            var videoURL = videoIFrame.attr('src');
+            videoIFrame.attr('src', videoURL);
+        }
+    });
+ 
+});
+
+
+/* Carousel Multi Item Los Gamarra */	
+jQuery(document).ready(function() {		
+	$('#gallery-Gamarra').on('slide.bs.carousel', function (e) {
+	    /*
+	        CC 2.0 License Iatek LLC 2018
+	        Attribution required
+	    */
+	    var $e = $(e.relatedTarget);
+	    var idx = $e.index();
+	    var itemsPerSlide = 4;
+	    var totalItems = $('#gallery-Gamarra .carousel-item').length;
+	    
+	    if (idx >= totalItems-(itemsPerSlide-1)) {
+	        var it = itemsPerSlide - (totalItems - idx);
+	        for (var i=0; i<it; i++) {
+	            // append slides to end
+	            if (e.direction=="left") {
+	                $('#gallery-Gamarra .carousel-item').eq(i).appendTo('#gallery-Gamarra .carousel-inner');
+	            }
+	            else {
+	                $('#gallery-Gamarra .carousel-item').eq(0).appendTo('#gallery-Gamarra .carousel-inner');
+	            }
+	        }
+	    }
+	});
+	
+});
+
+//Carrousel Modal Los Gamarra//
+jQuery(document).ready(function() { 
+    /*
+        Stop video playing when the MODAL is being closed (has finished closing)
+    */
+    $('#modal-Gamarra').on('hidden.bs.modal', function(e) {
+        $('#modal-Gamarra iframe').each(function() {
+            var videoURL = $(this).attr('src');
+            $(this).attr('src', videoURL);
+        });
+    });
+ 
+});
+
+jQuery(document).ready(function() { 
+    /*
+        Stop video playing when the CAROUSEL slides to another element
+    */
+    $('#carousel-Gamarra').on('slid.bs.carousel', function(e) {
+        var currentSlide = $('#carousel-Gamarra .carousel-item').eq(e.from);
+        var currentSlideEmbed = currentSlide.children('.embed-responsive');
+        if(currentSlideEmbed.length > 0) {
+            var videoIFrame = currentSlideEmbed.children('iframe');
+            var videoURL = videoIFrame.attr('src');
+            videoIFrame.attr('src', videoURL);
+        }
+    });
+ 
+});
+
+
+
+
+
+
