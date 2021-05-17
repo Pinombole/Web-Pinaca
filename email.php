85abc547f77b6ad551eab5759e077c7b04eeb5c6
Emmanuel Carmona <pinombole@gmail.com>
2021-05-17 16:39:59 -0300

    prueba emailphp con tsl

--- email.php
+++ email.php
@@ -19,7 +19,7 @@ try {
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'pinombole.usa@gmail.com';                     //SMTP username
     $mail->Password   = 'putoelquelee127';                         //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
