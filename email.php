c88a6ba5f51921eaf3443c01771bd719c112ab7b
Emmanuel Carmona <pinombole@gmail.com>
2021-05-17 17:33:32 -0300

    emailphp restaurado

--- email.php
+++ email.php
@@ -17,10 +17,11 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'pinombole.usa@gmail.com';                     //SMTP username
-    $mail->Password   = 'putoelquelee127';                         //SMTP password
-    $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Username   = 'pinombole.usa@gmail.com';                 //SMTP username
+    $mail->Password   = 'putoelquelee127';                         //SMTP password
+
 
     //Recipients
     $mail->setFrom('pinombole.usa@gmail.com', 'Pinaca Usa'); // Hacer coincidir con el username. (preferentemente)
