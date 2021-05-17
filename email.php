a490be9c5fddb121e150a186f6da8ce6311c0fd0
Emmanuel Carmona <pinombole@gmail.com>
2021-05-17 17:44:29 -0300

    emailphp puerto 465

--- email.php
+++ email.php
@@ -17,7 +17,7 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Username   = 'pinombole.usa@gmail.com';                 //SMTP username
     $mail->Password   = 'putoelquelee127';                         //SMTP password
